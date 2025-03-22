7299e53e72255b1aea72f8ebffb7297259974167
Sandra C <schoysm@gmail.com>
2025-03-22 19:57:54 +0800

    update max image size to 10MB, add autorefresh to admin attendance list, edit app.run

--- Webserver/Models/employee.py
+++ Webserver/Models/employee.py
@@ -6,7 +6,7 @@ class Employee(db.Model):
     employee_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     full_name = db.Column(db.String(255), nullable=False)
     department = db.Column(db.String(100), nullable=False)
-    profile_pic = db.Column(db.LargeBinary, nullable=False)  # BLOB type for image
+    profile_pic = db.Column(db.LargeBinary(length=(16 * 1024 * 1024)), nullable=False)  # 16 MB  # BLOB type for image
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)  # Store hashed password
 
